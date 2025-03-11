bcd3a0894f0effa7bcd9fa35d8d381e2ecba3e47
Bhavy <bhavyajain817@gmail.com>
2025-03-11 15:30:18 +0530

    changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -83,7 +83,7 @@ export default function Footer() {
                   href="/blogs"
                   className="hover:text-white transition-colors"
                 >
-                  Advocate's Insights
+                  Advocate&apos;s Insights
                 </Link>
               </li>
             </ul>
