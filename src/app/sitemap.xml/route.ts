cf54467504fdac3d37793b0fa72c6624d25d12e8
Bhavy <bhavyajain817@gmail.com>
2025-12-09 16:20:09 +0530

    changes

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -101,6 +101,7 @@ export async function GET(): Promise<Response> {
     'services/loan-settlement/axis-bank',
     'services/loan-settlement/kisetsu-saison-finance',
     'services/loan-settlement/indusind-bank',
+    'services/loan-settlement/smfg-india-credit',
     '/services/debt-consolidation',
     // Specific Banking and Finance State Pages
     '/services/banking-and-finance/andhra-pradesh',
