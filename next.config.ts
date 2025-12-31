a2688cf2a9f13b3451b92fc28f5ca37ef3e926a9
Bhavy <bhavyajain817@gmail.com>
2025-12-31 11:38:24 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -149,19 +149,7 @@ const nextConfig = {
       },
     ];
   },
-  async headers() {
-    return [
-      {
-        source: '/:all*(svg|jpg|png)',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-    ];
-  },
+
 };
 
 export default nextConfig;
