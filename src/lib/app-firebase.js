7c7f9e05ad73f9f10a5fca56a7cc8f7e69394b5a
Bhavy <bhavyajain817@gmail.com>
2026-01-17 13:21:51 +0530

    changes

--- src/lib/app-firebase.js
+++ src/lib/app-firebase.js
@@ -14,21 +14,13 @@ import { getAnalytics } from "firebase/analytics";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-
-  apiKey: "AIzaSyBSeECr8rLsXuiWXDujOLRDU8c3FiLF8fM",
-
-  authDomain: "amalegal-app.firebaseapp.com",
-
-  projectId: "amalegal-app",
-
-  storageBucket: "amalegal-app.firebasestorage.app",
-
-  messagingSenderId: "7012510059",
-
-  appId: "1:7012510059:web:e2b76b8d0c2dcd62c5442e",
-
-  measurementId: "G-24KPP0MLEN"
-
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 
