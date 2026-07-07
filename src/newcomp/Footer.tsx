d30d90003acb3df82262b70e47862967fe6cc368
Bhavy <bhavyajain817@gmail.com>
2026-07-07 10:39:13 +0530

    changes

--- src/newcomp/Footer.tsx
+++ src/newcomp/Footer.tsx
@@ -1026,7 +1026,7 @@ const Footer = () => {
         { name: "Trademark Registration Timeline", href: "/how-much-time-it-takes-to-get-your-trademark-registered" },
         { name: "Expedited Trademark Process", href: "/expedited-process-for-trademark-registration" },
         { name: "Trademark in 180 Days", href: "/get-your-trademark-in-180-days" },
-        { name: ".", href: "https://www.proedgeconsultation.in", hiddenLink: true }
+
       ]
     },
     {
